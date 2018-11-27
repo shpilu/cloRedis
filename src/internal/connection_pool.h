e51d396e3f807a69df7b59bb6157a5b1d1161033
shpilu <weijianlhp@163.com>
2018-11-27 18:18:44 +0800

    rewrite connection pool template

--- src/internal/connection_pool.h
+++ src/internal/connection_pool.h
@@ -1,17 +1,43 @@
 // 
-// General connection pool implementation
-// To some extent inspired by redigo (https://github.com/gomodule/redigo)
+// Generic pool implementation
+// with support for both connection pool and memory pool
+// multithread-safe
+// to some exent expired by redigo(https://github.com/gomodule/redigo.git)
 // Copyright 2017 James Wei (weijianlhp@163.com)
 //
 
 #ifndef CLORIS_CONNECTION_POOL_H_
 #define CLORIS_CONNECTION_POOL_H_
 
+#include <unistd.h>
+#include <time.h>
+#include <mutex>
+#include <forward_list>
+#include <functional>
+
+#define NUMBER_UNLIMITED -1
+
 namespace cloris {
+
+static uint64_t __get_current_time_ms() {
+    struct timeval tv; 
+    if (gettimeofday(&tv, NULL)) {   
+        return 0;
+    }
+    return tv.tv_sec * 1000 + tv.tv_usec / 1000 ;
+}
 
 template <typename T>
 struct ListItem {
-    T* obj;
+    ListItem() 
+        : active_time(__get_current_time_ms()),
+          create_time(__get_current_time_ms()),
+          next(NULL),
+          prev(NULL) { 
+    }
+    ~ListItem() { }
+    T* GetObject() { return reinterpret_cast<T*>(this + 1); }
+
     int64_t active_time; 
     int64_t create_time;
     ListItem<T> *next;
@@ -20,6 +46,19 @@ struct ListItem {
 
 template <typename T>
 struct IdleList {
+    IdleList() 
+        : count(0), 
+          front(NULL), 
+          back(NULL) { 
+    }
+    ~IdleList() {
+        for (ListItem<T> *tobj = front; tobj != NULL; tobj = tobj->next) {
+            T* t = tobj->GetObject();
+            t->~Type();
+            tobj->~ListItem<Type>();
+            free(tobj);
+        }
+    }
     void PushFront(ListItem<T> *item);
     void PopFront();
     void PopBack();
@@ -81,45 +120,164 @@ void IdleList<T>::PopBack() {
     return;
 }
 
-class GenericConnection {
-public:
-    virtual void GenericConnection() {}
-    virtual void ~GenericConnection() {}
-private:
+struct ConnectionPoolOption {
+    ConnectionPoolOption() 
+        : max_idle(NUMBER_UNLIMITED),
+          max_active(NUMBER_UNLIMITED),
+          idle_timeout_ms(NUMBER_UNLIMITED),
+          max_conn_life_time(NUMBER_UNLIMITED) {
+      }
+
+    int max_idle;
+
+    // Maximum number of connections allocated by the pool at a given time
+    int max_active;
+    int64_t idle_timeout_ms;
+    int64_t max_conn_life_time;
 };
 
-template <class GenericType, class Type> 
+struct ConnectionPoolStats {
+    ConnectionPoolStats() : malloc_fail_(0), overload_error(0) { }
+
+    int malloc_fail_;
+    int overload_error;
+};
+
+
+template <typename Type> 
 class ConnectionPool {
+    typedef std::function<void(void)> InitHandler;
 public:
-   void ConnectionPool(); 
-   
-   void Connect();
-   void TestOnBorrow();
-   Type* Get(int partition = 0);
-   void Put(Type* type);
-   void Close();
+    ConnectionPool(InitHandler ihandler = NULL) : init_handler_(NULL), active_cnt_(0) { }
+    ConnectionPool(const ConnectionPoolOption& option, InitHandler ihandler = NULL) 
+        : init_handler_(NULL), 
+          option_(option), 
+          active_cnt_(0) { 
+    }
+    ~ConnectionPool(); 
+    Type* Get();
+    void Put(Type* type);
 
 private:
-    int max_idle_;
-    int max_active_;
-    int64_t idle_timeout_ms_:
-    int64_t max_conn_life_time_;
-    bool closed_;
-    int  active_cnt_:
-    int  partition_cnt_;
-    IdleList<Type>* idle_;
+    Type* GetNewInstance();
+    void Gc(Type* type);
+
+    InitHandler init_handler_;
+    ConnectionPoolOption option_;
+    ConnectionPoolStats stats_;
+    IdleList<Type> idle_;
+    std::forward_list<ListItem<Type>*> mem_pool_;
+    // Number of connections allocated by the pool at a given time.
+    int active_cnt_;
+    std::mutex mutex_;
+    std::mutex pool_mtx_;
 };
 
-template<class GenericType, class Type>
-ConnectionPool<GenericType, Type>::ConnectionPool() {
-   // check if Type is child class of GenericType in compile time
-   GenericType* conn = (Type*)NULL;
-   (void)conn;
+template<typename Type>
+ConnectionPool<Type>::~ConnectionPool() {
+    ListItem<Type*> ptr(NULL); 
+    while (!mem_pool_.empty()) {
+        ptr = mem_pool_.front();
+        mem_pool_.pop_front();
+        free(ptr);
+    }
 }
 
+template<typename Type>
+Type* ConnectionPool<Type>::Get() {
+    if (option_.max_active > 0 && active_cnt_ >= option_.max_active) {
+        ++stats_.overload_error;
+        return NULL;
+    }
+    std::unique_lock<std::mutex> lck(mutex_, std::defer_lock);
+    if (option_.idle_timeout_ms > 0) {
+        lck.lock();
+        for (ListItem<Type*> item = idle_.back; 
+             (item != NULL) && (item.active_time + option_.idle_timeout_ms < __get_current_time_ms()); 
+             item = idle_.back) {
+             idle_.PopBack();
+             lck.unlock();
+             Gc(item);
+             lck.lock();
+        }
+        lck.unlock();
+    }
+    lck.lock();
+    for (ListItem<Type*> item = idle_.front; item != NULL; item = idle_.front) {
+        idle_.PopFront();
+        if ((option_.max_conn_life_time <= 0) || (__get_current_time_ms() - item.create_time < item.max_conn_life_time)) {
+            // TODO
+            lck.unlock();
+            return item->GetObject(); 
+        }
+        lck.unlock();
+        Gc(item);
+        lck.lock();
+    }
+    lock.unlock();
+    return GetNewInstance();
+}
 
+template<typename Type>
+Type* ConnectionPool<Type>::GetNewInstance() {
+    ListItem<Type>* ptr(NULL);
+    {
+        std::lock_guard<std::mutext> lk(pool_mtx_);
+        if (!mem_pool_.empty()) {
+            ptr = mem_pool_.front();
+            mem_pool_.pop_front();
+        }
+    }
+    if (!ptr) {
+        ptr = malloc(sizeof(ListItem<Type>) + sizeof(Type));
+        if (!ptr) {
+            ++stats_.malloc_fail_;
+            return NULL;
+        }
+    }
+    __sync_fetch_and_add(&active_cnt_, 1);
+    // placement new
+    new(ptr) ListItem;
+    Type* obj_ptr = reinterpret_cast<Type*>(ptr + 1);
+    new(obj_ptr) Type;
+    if (init_handler_) {
+        if (!init_handler_(obj_ptr)) {
+            Gc(obj_ptr);
+            return NULL;
+        }
+    }
+    return obj_ptr;
+}
 
+template<typename Type>
+ConnectionPool<Type>::Gc(Type* type) {
+    type->~Type();
+    ListItem<Type> *item = reinterpret_cast<ListItem<Type>>(type) - 1;
+    item->~ListItem<Type>();
+    {
+        std::lock_guard<std::mutex> lk(pool_mtx_);
+        mem_pool_.push_front(item);
+    }
+    __sync_fetch_and_sub(&active_cnt_, 1);
+}
 
+template<typename Type>
+ConnectionPool<Type>::Put(Type* type) {
+    ListItem<Type> *item = reinterpret_cast<ListItem<Type>>(type) - 1;
+    item->active_time = __get_current_time_ms();
+    ListItem<Type> *idl(NULL);
+    {
+        std::lock_guard<std::mutext> lk(mutex_);
+        idle_.PushFront(item);
+        if ((option_.max_idle > 0) && (idle_.count > option_.max_idle)) {
+            idl = idle_.back;
+            idle_.PopBack(); 
+        }
+    }
+    if (idl) {
+        Gc(idl);
+    }
+}
 
 } // namespace cloris
 
